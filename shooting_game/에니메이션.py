21f2db2fd0ac2bb599cc63fb0c3687cc1a262695
zino1187 <zino1187@gmail.com>
2020-12-06 11:57:25 +0900

    c

--- shooting_game/에니메이션.py
+++ shooting_game/에니메이션.py
@@ -1,9 +1,15 @@
 
 from tkinter import * 
-win=Tk() #윈도우 생성함수 
 
+# next함수 정의하기 
+def next():
+    print("나 눌렀어?")
+
+win=Tk() #윈도우 생성함수 
 canvas=Canvas(win, width=800, height=800, bg="yellow")
-bt=Button(win, text="다음사진")
+
+#버튼을 누를때 next라는 코드를 호출한다!!(즉 함수를 호출한다..)
+bt=Button(win, text="다음사진", command=next)
 
 canvas.pack()
 bt.pack()

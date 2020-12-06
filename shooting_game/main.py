368fd296c07cae3e70f9944fda2b29c048b51148
zino1187 <zino1187@gmail.com>
2020-12-06 11:12:54 +0900

    c

--- shooting_game/main.py
+++ shooting_game/main.py
@@ -5,13 +5,17 @@
  #x=x+3 #기존의 x라는 공간의 값에 3을 더하여 다시 x에 대입해
 from tkinter import *  #tkinter라는 모듈(GUI)중의 모든 함수를 다 참고하기!!
 from PIL import Image # PIL 모듈의 Image 라는 클래스를 가져오자
+from PIL import ImageTk
 
 # 생성하기
 win=Tk() #윈도우 화면 호출 
 # 켄버스를 만들고, 만들어진 켄버스를 canvas변수로 부르자!!
 canvas=Canvas(win, width=1460, height=800, bg="yellow")
-Image.open("경로")
 
+img=Image.open("./images/sky.png")
+# 이미지 사이즈 원하는 크기로 재조정 
+img=img.resize( (1460,800), Image.ANTIALIAS)
+img=ImageTk.PhotoImage(img)
 
 #윈도우창의 크기를 원하는크기로 지정 
 win.geometry("1460x800") #가로(너비)x세로 (높이)
